e94f88f56c246f084b68f74db833a1f8d05e2d3d
Martin Bernát <ber39288@spseol.cz>
2022-05-10 21:59:03 +0200

    První neúspěšný pokus o pochopení UZ senzoru

--- app/src/main.c
+++ app/src/main.c
@@ -1,5 +1,9 @@
 #include "stm8s.h"
 
+void usDelay(uint32_t uSec)
+{
+}
+
 void main(void)
 {
     CLK_HSIPrescalerConfig(CLK_PRESCALER_HSIDIV1); // FREQ MCU 16MHz
@@ -7,8 +11,14 @@ void main(void)
 
     while (1)
     {
+        usDelay(10);
+        while (1)
+        {
+        TIM3_TimeBaseInit(TIM3_PRESCALER_256, 62499);
+        TIM3_Cmd(ENABLE);
+        while (TIM3_GetFlagStatus(TIM3_FLAG_UPDATE) != SET);
+        TIM3_ClearFlag(TIM3_FLAG_UPDATE);
         GPIO_WriteReverse(GPIOC, GPIO_PIN_5);
-        for (uint32_t i = 0; i < 100000; i++)
-            ;
+        }
     }
 }
